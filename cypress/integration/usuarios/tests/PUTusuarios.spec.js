fe1bec01ddaebc8b5793e4547371c3f62b05a62d
guilhermelemke <guilherme.lemke@gmail.com>
2021-01-18 13:48:25 -0300

    Last put added to test

--- cypress/integration/usuarios/tests/PUTusuarios.spec.js
+++ cypress/integration/usuarios/tests/PUTusuarios.spec.js
@@ -1,6 +1,7 @@
 import { registerUser } from '../requests/POSTusuarios.request';
 import { editUser } from '../requests/PUTusuarios.request';
 import { deleteUser } from '../requests/DELETEusuarios.request';
+import { getAllUsers } from '../requests/GETusuarios.request';
 
 let user;
 
@@ -24,27 +25,7 @@ describe('PUT usuarios', () => {
             })
         })    
 
-    // it('Tenta editar usuario', () => {
-    //     registerUser({
-    //         nome: user.username,
-    //         email: user.email,
-    //         password: user.password,
-    //         administrador: "true"
-    //     }).then(responseUser => {
-    //         editUser(responseUser.body._id, {
-    //                 nome: user.username,
-    //                 email: user.email,
-    //                 password: user.password,
-    //                 administrador: "true"
-    //             }).should(response => {
-    //                 expect(response.status).to.eq(400);
-    //                 expect(response.body.message).to.eq("Este email já está sendo usado");
-    //                 deleteUser(response.body._id)
-    //         })    
-    //     })
-    //})
-
-    it.only('Edita usuário', () => {
+    it('Edita usuário', () => {
         registerUser({
             nome: user.username,
             email: user.email,
@@ -53,14 +34,28 @@ describe('PUT usuarios', () => {
         }).then(responseUser => {
             editUser(responseUser.body._id, {
                 nome: user.username,
-                email: "user@email.iof",
+                email: user.email,
                 password: user.password,
                 administrador: "true"
             }).should(response => {
                 expect(response.status).to.eq(200);
-                expect(response.body.message).to.eq("Este email já está sendo usado");
+                expect(response.body.message).to.eq("Registro alterado com sucesso");
+                deleteUser(responseUser.body._id)
             })   
-            deleteUser(responseUser.body._id) 
         })
-    }) 
+    })
+
+    it('Tenta editar usuário (alterar email para um já existente)', () => {
+        getAllUsers().then(responseUsers => {
+            editUser(responseUsers.body.usuarios[0]._id, {
+                nome: user.username,
+                email: responseUsers.body.usuarios[1].email,
+                password: user.password,
+                administrador: "true"
+            }).should(response => {
+                expect(response.status).to.eq(400);
+                expect(response.body.message).to.eq("Este email já está sendo usado")
+            })
+        })
+    })
 })
