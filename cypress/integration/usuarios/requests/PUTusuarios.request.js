3eb3f3b691b4f557039c6fe37c317b899061e5ec
guilhermelemke <guilherme.lemke@gmail.com>
2021-01-18 08:57:46 -0300

    Project without fake generator

--- cypress/integration/usuarios/requests/PUTusuarios.request.js
+++ cypress/integration/usuarios/requests/PUTusuarios.request.js
@@ -1,6 +1,7 @@
 /// <reference types="cypress"/>
 
 const payloadUser = require('../payloads/edit-user.json');
+const payloadEditUser = require('../payloads/edit-user-email.json');
 
 function editUser(id) {
     return cy.request({
@@ -11,4 +12,25 @@ function editUser(id) {
     })
 }
 
-export { editUser };
+function editExistingUser(jsonBody) {
+    let json = jsonBody;
+    const id = json["_id"];
+    delete json["_id"];
+    return cy.request({
+        method: 'PUT',
+        url: `usuarios/${id}`,
+        failOnStatusCode: false,
+        body: jsonBody
+    })
+}
+
+function editScucessfulUser(id) {
+    return cy.request({
+        method: 'PUT',
+        url: `usuarios/${id}`,
+        failOnStatusCode: false,
+        body: payloadEditUser
+    })
+}
+
+export { editUser, editExistingUser, editScucessfulUser };
--- cypress/integration/usuarios/tests/PUTusuarios.spec.js
+++ cypress/integration/usuarios/tests/PUTusuarios.spec.js
@@ -1,35 +1,33 @@
 import { registerUser } from '../requests/POSTusuarios.request';
-import { editUser } from '../requests/PUTusuarios.request';
+import { editUser, editExistingUser, editScucessfulUser } from '../requests/PUTusuarios.request';
 import { deleteUser } from '../requests/DELETEusuarios.request';
+import { getAllUsers } from '../requests/GETusuarios.request';
 
 describe('PUT usuarios', () => {
     it('Cadastra usuário por não existir id', () => {
-            editUser("sedsdfgasdfergretrh").should(response => {
-                expect(response.status).to.eq(201);
-                expect(response.body.message).to.eq("Cadastro realizado com sucesso");
-                deleteUser(response.body._id)
-            })
-        })    
+        editUser("sedsdffergretrh").should(response => {
+            expect(response.status).to.eq(201);
+            expect(response.body.message).to.eq("Cadastro realizado com sucesso");
+            deleteUser(response.body._id)
+        })
+    })    
 
-
-        // Testes a fazer:
     // it('Tenta editar usuario', () => {
-    //     registerUser().then(responseUser => {
-    //         editUser(responseUser.body._id).should(response => {
-    //             expect(response.status).to.eq(400);
-    //             expect(response.body.message).to.eq("Este email já está sendo usado");
-    //             deleteUser(response.body._id)
-    //         })    
+    //     getAllUsers().then(response => {
+    //         editExistingUser(response.body.usuarios[0]).should(validation => {
+    //             expect(validation.status).to.eq(400);
+    //             expect(validation.body.message).to.eq("Este email já está sendo usado");        
+    //         })
     //     })
     // })
 
-    //it('Edita usuário', () => {
-        //registerUser().then(responseUser => {
-            //editUser(responseUser.body._id).should(response => {
-                //expect(response.status).to.eq(200);
-                //expect(response.body.message).to.eq("Este email já está sendo usado");
-            //})   
-            //deleteUser(responseUser.body._id) 
-        //})
-    //}) 
+    // it('Edita usuário', () => {
+    //     registerUser().then(responseUser => {
+    //         editScucessfulUser(responseUser.body._id, ).should(response => {
+    //             expect(response.status).to.eq(200);
+    //             expect(response.body.message).to.eq("Registro alterado com sucesso");
+
+    //         })   
+    //     })
+    // }) 
 })
